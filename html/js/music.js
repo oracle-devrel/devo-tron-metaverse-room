3ad8d2202726ae9e83c4431735052d17d4e7c5dd
WSPluta <60693402+WSPluta@users.noreply.github.com>
2022-07-08 16:30:16 +0100

    index updates

--- html/js/music.js
+++ html/js/music.js
@@ -1,3 +1,4 @@
+ //bg music handler
  document.querySelector("button").onclick = function() {
      document.querySelector("#loadingScreen").style.display = "none"
      document.querySelector("#bgSoundtrack").volume = 0.09;
