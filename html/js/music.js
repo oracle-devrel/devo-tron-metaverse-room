5f9d8ccfc0a4e86c4b9371396bdd3c9879cf7cbf
WSPluta <60693402+WSPluta@users.noreply.github.com>
2022-07-08 12:41:44 +0100

    added audio

---
+++ html/js/music.js
@@ -0,0 +1,5 @@
+ document.querySelector("button").onclick = function() {
+     document.querySelector("#loadingScreen").style.display = "none"
+     document.querySelector("#bgSoundtrack").volume = 0.09;
+     document.querySelector("#bgSoundtrack").play()
+   };
